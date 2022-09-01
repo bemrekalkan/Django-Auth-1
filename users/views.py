661d3b687ea0eb6f51296a8c05e33df57c005f2f
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-01 21:39:49 +0300

    ..

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
-
+from django.contrib.auth import logout
 from django.contrib import messages
-
 # Create your views here.
-
 def home(request):
     return render(request, 'users/home.html')
+def user_logout(request):
+    logout(request)
+    return redirect('home')
